133c120654fdcbabb9a5418e44e4437e2f5204d6
David Gilson <d.gilson@afelio.be>
2020-11-30 13:57:19 +0100

    change detectChange by markDirty

--- src/utils/refresh-view-on-change.operator.ts
+++ src/utils/refresh-view-on-change.operator.ts
@@ -1,10 +1,14 @@
-import { ɵdetectChanges as detectChanges } from '@angular/core';
+import {
+  // ɵdetectChanges as detectChanges,
+  ɵmarkDirty as markDirty
+} from '@angular/core';
 import { tap } from 'rxjs/operators';
 
 export const refreshViewOnChanges = <T>(cmpt) => {
     return tap<T>(() => {
       setTimeout(() => {
-        detectChanges(cmpt);
+        // detectChanges(cmpt);
+        markDirty(cmpt);
       });
     });
 };
